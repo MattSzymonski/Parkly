6b93d794d1d488804f31c6074ab85b795bd8ef8b
DanielPlawiak <plawiak.daniel@gmail.com>
2021-02-06 14:01:24 -0800

    working paginaton, list, adding form

---
+++ App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import LoginPage from './components/loginPage';
+import ListPage from './components/listPage';
+import AddEntry from './components/addEntry';
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer, StackActions} from '@react-navigation/native'
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return ( <NavigationContainer style={styles.container}>
+    
+      {/* <LoginPage>
+      
+        </LoginPage> */}
+        {/* <ListPage>
+          </ListPage> */}
+
+    
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name = "Login" component={LoginPage} />
+        <Stack.Screen name = "List" component={ListPage} />
+        <Stack.Screen name = "Add" component={AddEntry} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
