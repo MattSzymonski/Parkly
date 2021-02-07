8ffaa05a31deee44d0653002f4f713974737fbad
DanielPlawiak <57326838+DanielPlawiak@users.noreply.github.com>
2021-02-07 15:10:27 +0100

    Update detailsPage.js

--- MobileApp/components/detailsPage.js
+++ MobileApp/components/detailsPage.js
@@ -13,21 +13,21 @@ import axios from 'axios';
 
 const api_url = "http://parkly-env.eba-u2qumtf7.us-east-2.elasticbeanstalk.com";
 
-export default function DetailsPage({navigation, route}) {
+const DetailsPage = ({navigation, route}) => {
 
-    const [name, setName] = useState(route.params.name);
-    const [country, setCountry] = useState(route.params.country);
-    const [town, setTown] = useState(route.params.town);
-    const [streetName, setStreet] = useState(route.params.streetName);
-    const [streetNumber, setStreetNumber] = useState(route.params.streetNumber);
-    const [spotsTotal, setSpotsTotal] = useState(route.params.spotsTotal);
-    const [firstName, setFirstName] = useState(route.params.firstName);
-    const [lastName, setLasttName] = useState(route.params.lastName);
-    const [companyName, setCompanyName] = useState(route.params.companyName);
-    const [ownerCountry, setOwnerCountry] = useState(route.params.ownerCountry);
-    const [ownerTown, setOwnerTown] = useState(route.params.ownerTown);
-    const [ownerStreet, setOwnerStreet] = useState(route.params.ownerStreet);
-    const [ownerStreetNumber, setOwnerStreetNumber] = useState(route.params.ownerStreetNumber);
+    const [name, setName] = useState(route.params.item.name);
+    const [country, setCountry] = useState(route.params.item.country);
+    const [town, setTown] = useState(route.params.item.town);
+    const [streetName, setStreet] = useState(route.params.item.streetName);
+    const [streetNumber, setStreetNumber] = useState(route.params.item.streetNumber);
+    const [spotsTotal, setSpotsTotal] = useState(route.params.item.spotsTotal);
+    const [firstName, setFirstName] = useState(route.params.item.firstName);
+    const [lastName, setLasttName] = useState(route.params.item.lastName);
+    const [companyName, setCompanyName] = useState(route.params.item.companyName);
+    const [ownerCountry, setOwnerCountry] = useState(route.params.item.ownerCountry);
+    const [ownerTown, setOwnerTown] = useState(route.params.item.ownerTown);
+    const [ownerStreet, setOwnerStreet] = useState(route.params.item.ownerStreet);
+    const [ownerStreetNumber, setOwnerStreetNumber] = useState(route.params.item.ownerStreetNumber);
 
     const [isEditingAddress, setAddressEditing] = useState(false);
     const [err, setErr] = useState("");
@@ -39,7 +39,7 @@ export default function DetailsPage({navigation, route}) {
     }
 
     const updateParking = () => {
-        axios.put(`${api_url}/p/parkings/${parking.id}?createNewOwner=false`, {
+        axios.put(`${api_url}/p/parkings/${route.params.item.id}?createNewOwner=false`, {
             name: name,
             address: {
                 country: country,
@@ -60,7 +60,7 @@ export default function DetailsPage({navigation, route}) {
                 }
             }
         },
-        options).then(response => navigation.navigate('List', {token: route.params.token, user: route.params.username})).catch(err => setErr(err))
+        options).then((response) => navigation.navigate('List', {token: route.params.token, user: route.params.username})).catch(err => setErr(err))
     }
 
     return (
@@ -82,11 +82,12 @@ export default function DetailsPage({navigation, route}) {
                 <TextInput value={name} onChangeText={(val) => setName(val)}></TextInput>}
             </View>
 
-            <View>
-                <TouchableOpacity onPress={updateParking}>
+     
+                <TouchableOpacity onPress={updateParking} style={{borderRadius: 20, backgroundColor: "yellow", height: 40, width: 100, margin: 35}}>
                     <Text>Update</Text>
+                   
                 </TouchableOpacity>
-            </View>
+  
         </View>
     )
 
@@ -103,3 +104,5 @@ const styles = StyleSheet.create({
         backgroundColor: "#ffd300"
     },
 });
+
+export default DetailsPage;
